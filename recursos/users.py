c685fa6bec2a84ce0846babd36e93f2fd7b32ae0
Bran Stark <danielprata@outlook.com>
2018-12-06 14:10:53 -0200

    interacao mongodb crud

--- recursos/users.py
+++ recursos/users.py
@@ -1,36 +1,44 @@
 from flask import Blueprint, jsonify, request
 from modulos.mongodb import *
 
+
 def validar_json(data):
     for value in data.values():
         if not value:
             return False
     else:
         return True
-            
+
+
 db = connect_mongodb()
 
 
 usuarios = Blueprint('usuarios', __name__)
+
 
 @usuarios.route("/usuarios", methods=["GET"])
 def all_users():
     return jsonify(list(db.usuarios.find()))
 
 
-@usuarios.route("/usuario/<int:num>", methods=["GET"] )
+@usuarios.route("/usuario/<int:num>", methods=["GET"])
 def one_user(num):
-    return jsonify(db.usuarios.find_one({"_id":num}))
+    return jsonify(db.usuarios.find_one({"_id": num}))
+
 
 @usuarios.route("/usuario/<int:num>", methods=["POST"])
 def insert_user(num):
-    data = request.get_json()
-    status = validar_json(data)
-    if data and status:
-        # Cadastrar no mongodb
-        return jsonify({"status": status})
-    else:
-        return jsonify({ "status": False})
+    try:
+        data = request.get_json()
+        status = validar_json(data)
+        if data and status:
+            data["_id"] = num
+            db.usuarios.insert_one(data)
+            return jsonify({"status": status})
+        else:
+            raise Exception("Falha ao cadastrar!")
+    except Exception as e:
+        return jsonify({"status": False})
 
 
 @usuarios.route("/usuario/<int:num>", methods=["PUT"])
@@ -38,11 +46,13 @@ def update_user(num):
     data = request.get_json()
     status = validar_json(data)
     if data and status:
-        # atualizar no mongodb
+        db.usuarios.update_one({"_id": num}, {"$set": data})
         return jsonify({"status": status})
     else:
-        return jsonify({ "status": False})
+        return jsonify({"status": False})
+
 
 @usuarios.route("/usuario/<int:num>", methods=["DELETE"])
 def delete_user(num):
-    return jsonify({"status":True})
+    db.usuarios.remove({"_id": num})
+    return jsonify({"status": True})
